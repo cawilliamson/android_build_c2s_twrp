72597aa84b91ce66c9ddf16fd075a0d3bdda4d9d
Christopher A. Williamson <home@chrisaw.com>
2020-08-29 18:56:53 +0100

    Refactored buildtwrp code to be cleaner

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 echo "[*] Building docker container"
 docker build -t chrisawcom/buildtwrp .
 
+# create out directory
+mkdir -p out/
+
 # docker run
 echo "[*] Starting TWRP build"
-docker run --privileged --tty --rm -v "$(pwd):/var/tmp/buildtwrp" -v "$(pwd)/src:/usr/src" chrisawcom/buildtwrp
+docker run --privileged --tty --rm -v "$(pwd):/var/tmp/buildtwrp" chrisawcom/buildtwrp
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,16 +10,18 @@ cd /usr/src/twrp
 # init repo
 repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0-WIP
 
+# apply fix-manifests patch
+patch -p1 --forward < /var/tmp/buildtwrp/patches/fix-manifests.patch || true
+
 # create extra file
 mkdir -p .repo/local_manifests/
-cp -fv /var/tmp/buildtwrp/remove-minimal.xml .repo/manifests/remove-minimal.xml
-cp -v /var/tmp/buildtwrp/extra.xml .repo/local_manifests/extra.xml
+cp -v /var/tmp/buildtwrp/manifests/extra.xml .repo/local_manifests/extra.xml
 
 # sync repo
 repo sync -c -j"$(nproc --all)" --force-sync --no-clone-bundle --no-tags
 
-# apply fix-relinks patch
-patch -p1 --forward < /var/tmp/buildtwrp/android10-support.patch || true
+# apply android-10 patch
+patch -p1 --forward < /var/tmp/buildtwrp/patches/android10-support.patch || true
 
 # run actual build
 export ALLOW_MISSING_DEPENDENCIES=true
@@ -28,5 +30,5 @@ lunch omni_c2s-eng
 mka -j$(nproc --all) recoveryimage
 
 # copy image to output dir
-cp -v out/target/product/c2s/recovery.img /var/tmp/buildtwrp/
+cp -v out/target/product/c2s/recovery.img /var/tmp/buildtwrp/out/
 chmod 777 /var/tmp/buildtwrp/recovery.img
