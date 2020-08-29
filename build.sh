e4ee029c5a1c80c65444f57dd4579e0829b7959e
Christopher A. Williamson <home@chrisaw.com>
2020-08-29 12:38:00 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# run docker build
+echo "[*] Building docker container"
+docker build -t chrisawcom/buildtwrp .
+
+# docker run
+echo "[*] Starting TWRP build"
+docker run --privileged --tty --rm -v "$(pwd):/var/tmp/buildtwrp" -v "$(pwd)/src:/usr/src" chrisawcom/buildtwrp
---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# set git preferences
+git config --global color.ui false
+
+# change to twrp build dir
+mkdir -p /usr/src/twrp
+cd /usr/src/twrp
+
+# init repo
+repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0-WIP
+
+# create extra file
+mkdir -p .repo/local_manifests/
+cp -fv /var/tmp/buildtwrp/remove-minimal.xml .repo/manifests/remove-minimal.xml
+cp -v /var/tmp/buildtwrp/extra.xml .repo/local_manifests/extra.xml
+
+# sync repo
+repo sync -c -j"$(nproc --all)" --force-sync --no-clone-bundle --no-tags
+
+# apply fix-relinks patch
+patch -p1 < /var/tmp/buildtwrp/cleanup-relinks.patch
+
+# run actual build
+export ALLOW_MISSING_DEPENDENCIES=true
+. build/envsetup.sh
+lunch omni_d2s-eng
+mka -j$(nproc --all) recoveryimage
+
+# copy image to output dir
+cp -v out/target/product/d2s/recovery.img /var/tmp/buildtwrp/
+chmod 777 /var/tmp/buildtwrp/recovery.img
