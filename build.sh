0a1dfe44b585a637b050b1944bf2850bc4b9d1f3
Christopher A. Williamson <home@chrisaw.com>
2020-08-29 18:58:11 +0100

    Remove useless status outputs

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 # run docker build
-echo "[*] Building docker container"
 docker build -t chrisawcom/buildtwrp .
 
 # create out directory
 mkdir -p out/
 
 # docker run
-echo "[*] Starting TWRP build"
 docker run --privileged --tty --rm -v "$(pwd):/var/tmp/buildtwrp" chrisawcom/buildtwrp
